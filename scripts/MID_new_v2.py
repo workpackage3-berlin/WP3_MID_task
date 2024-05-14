d97c03a3432642952702ed083e5347f9bf8aadd0
Charlotte Stensholt <charlottestensholt@hotmail.com>
2024-05-14 14:36:19 +0200

    Added blank screen before feedback

--- scripts/MID_new_v2.py
+++ scripts/MID_new_v2.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2023.2.3),
-    on Mon May 13 14:33:11 2024
+    on Tue May 14 14:28:39 2024
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -114,7 +114,7 @@ def setupData(expInfo, dataDir=None):
     thisExp = data.ExperimentHandler(
         name=expName, version='',
         extraInfo=expInfo, runtimeInfo=None,
-        originPath='/Users/charlotte/Dropbox/Charite_PhD/tasks/MID_45_jitter_break/MID_45_jitt_break.py',
+        originPath='/Users/charlotte/Dropbox/Charite_PhD/tasks/MID_new_version/MID_new_version.py',
         savePickle=True, saveWideText=True,
         dataFileName=dataDir + os.sep + filename, sortColumns='time'
     )
@@ -413,6 +413,8 @@ def run(expInfo, thisExp, win, inputs, globalClock=None, thisSession=None):
         opacity=None, depth=-1.0, interpolate=True)
     ButtonPressTarget_training = keyboard.Keyboard()
     
+    # --- Initialize components for Routine "Blank_screen_training" ---
+    
     # --- Initialize components for Routine "FeedbackCode_training" ---
     text_Feedback_training = visual.TextStim(win=win, name='text_Feedback_training',
         text='',
@@ -498,6 +500,8 @@ def run(expInfo, thisExp, win, inputs, globalClock=None, thisSession=None):
         lineWidth=1.0,     colorSpace='rgb',  lineColor='white', fillColor='white',
         opacity=None, depth=-1.0, interpolate=True)
     ButtonPressTarget = keyboard.Keyboard()
+    
+    # --- Initialize components for Routine "Blank_screen" ---
     
     # --- Initialize components for Routine "FeedbackCode" ---
     text_Feedback = visual.TextStim(win=win, name='text_Feedback',
@@ -1808,6 +1812,69 @@ def run(expInfo, thisExp, win, inputs, globalClock=None, thisSession=None):
         # the Routine "TargetPresentation_training" was not non-slip safe, so reset the non-slip timer
         routineTimer.reset()
         
+        # --- Prepare to start Routine "Blank_screen_training" ---
+        continueRoutine = True
+        # update component parameters for each repeat
+        thisExp.addData('Blank_screen_training.started', globalClock.getTime())
+        # keep track of which components have finished
+        Blank_screen_trainingComponents = []
+        for thisComponent in Blank_screen_trainingComponents:
+            thisComponent.tStart = None
+            thisComponent.tStop = None
+            thisComponent.tStartRefresh = None
+            thisComponent.tStopRefresh = None
+            if hasattr(thisComponent, 'status'):
+                thisComponent.status = NOT_STARTED
+        # reset timers
+        t = 0
+        _timeToFirstFrame = win.getFutureFlipTime(clock="now")
+        frameN = -1
+        
+        # --- Run Routine "Blank_screen_training" ---
+        routineForceEnded = not continueRoutine
+        while continueRoutine and routineTimer.getTime() < 0.5:
+            # get current time
+            t = routineTimer.getTime()
+            tThisFlip = win.getFutureFlipTime(clock=routineTimer)
+            tThisFlipGlobal = win.getFutureFlipTime(clock=None)
+            frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+            # update/draw components on each frame
+            # is it time to end the Routine? (based on local clock)
+            if tThisFlip > 0.5-frameTolerance:
+                continueRoutine = False
+            
+            # check for quit (typically the Esc key)
+            if defaultKeyboard.getKeys(keyList=["escape"]):
+                thisExp.status = FINISHED
+            if thisExp.status == FINISHED or endExpNow:
+                endExperiment(thisExp, inputs=inputs, win=win)
+                return
+            
+            # check if all components have finished
+            if not continueRoutine:  # a component has requested a forced-end of Routine
+                routineForceEnded = True
+                break
+            continueRoutine = False  # will revert to True if at least one component still running
+            for thisComponent in Blank_screen_trainingComponents:
+                if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+                    continueRoutine = True
+                    break  # at least one component has not yet finished
+            
+            # refresh the screen
+            if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+                win.flip()
+        
+        # --- Ending Routine "Blank_screen_training" ---
+        for thisComponent in Blank_screen_trainingComponents:
+            if hasattr(thisComponent, "setAutoDraw"):
+                thisComponent.setAutoDraw(False)
+        thisExp.addData('Blank_screen_training.stopped', globalClock.getTime())
+        # using non-slip timing so subtract the expected duration of this Routine (unless ended on request)
+        if routineForceEnded:
+            routineTimer.reset()
+        else:
+            routineTimer.addTime(-0.500000)
+        
         # --- Prepare to start Routine "FeedbackCode_training" ---
         continueRoutine = True
         # update component parameters for each repeat
@@ -2644,7 +2711,7 @@ def run(expInfo, thisExp, win, inputs, globalClock=None, thisSession=None):
 
                 # Send LSL Marker : 
                 mark = 'Fixation2000'
-                outlet.push_sample([mark])  # Push event marker.     
+                outlet.push_sample([mark])  # Push event marker.   
 
                 # keep track of start time/frame for later
                 FixationScreen.frameNStart = frameN  # exact frame index
@@ -2832,11 +2899,11 @@ def run(expInfo, thisExp, win, inputs, globalClock=None, thisSession=None):
             
             # if TargetPresentationScreen is starting this frame...
             if TargetPresentationScreen.status == NOT_STARTED and tThisFlip >= 0.0-frameTolerance:
-                
+
                 # Send LSL Marker :
                 mark = 'Target'
-                outlet.push_sample([mark])  # Push event marker.                  
-                
+                outlet.push_sample([mark])  # Push event marker.      
+
                 # keep track of start time/frame for later
                 TargetPresentationScreen.frameNStart = frameN  # exact frame index
                 TargetPresentationScreen.tStart = t  # local t and not account for scr refresh
@@ -2968,7 +3035,8 @@ def run(expInfo, thisExp, win, inputs, globalClock=None, thisSession=None):
 
         if ButtonPressTarget.keys != None and EarlyPressFixation.keys == None and EarlyPressCue.keys == None:  # we had a response only at target routine
             MainLoop.addData('ButtonPressTarget.rt', ButtonPressTarget.rt)
-            MainLoop.addData('ButtonPressTarget.duration', ButtonPressTarget.duration)
+            MainLoop.addData('ButtonPressTarget.duration', ButtonPressTarget.duration)        
+
         # Run 'End Routine' code from codeFeedbacksaving
         feedbackver = [];
         # Used to decide feedback participants get based on cue (win, loss, neutral) and button press
@@ -3020,6 +3088,69 @@ def run(expInfo, thisExp, win, inputs, globalClock=None, thisSession=None):
             
         # the Routine "TargetPresentation" was not non-slip safe, so reset the non-slip timer
         routineTimer.reset()
+        
+        # --- Prepare to start Routine "Blank_screen" ---
+        continueRoutine = True
+        # update component parameters for each repeat
+        thisExp.addData('Blank_screen.started', globalClock.getTime())
+        # keep track of which components have finished
+        Blank_screenComponents = []
+        for thisComponent in Blank_screenComponents:
+            thisComponent.tStart = None
+            thisComponent.tStop = None
+            thisComponent.tStartRefresh = None
+            thisComponent.tStopRefresh = None
+            if hasattr(thisComponent, 'status'):
+                thisComponent.status = NOT_STARTED
+        # reset timers
+        t = 0
+        _timeToFirstFrame = win.getFutureFlipTime(clock="now")
+        frameN = -1
+        
+        # --- Run Routine "Blank_screen" ---
+        routineForceEnded = not continueRoutine
+        while continueRoutine and routineTimer.getTime() < 0.5:
+            # get current time
+            t = routineTimer.getTime()
+            tThisFlip = win.getFutureFlipTime(clock=routineTimer)
+            tThisFlipGlobal = win.getFutureFlipTime(clock=None)
+            frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+            # update/draw components on each frame
+            # is it time to end the Routine? (based on local clock)
+            if tThisFlip > 0.5-frameTolerance:
+                continueRoutine = False
+            
+            # check for quit (typically the Esc key)
+            if defaultKeyboard.getKeys(keyList=["escape"]):
+                thisExp.status = FINISHED
+            if thisExp.status == FINISHED or endExpNow:
+                endExperiment(thisExp, inputs=inputs, win=win)
+                return
+            
+            # check if all components have finished
+            if not continueRoutine:  # a component has requested a forced-end of Routine
+                routineForceEnded = True
+                break
+            continueRoutine = False  # will revert to True if at least one component still running
+            for thisComponent in Blank_screenComponents:
+                if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+                    continueRoutine = True
+                    break  # at least one component has not yet finished
+            
+            # refresh the screen
+            if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+                win.flip()
+        
+        # --- Ending Routine "Blank_screen" ---
+        for thisComponent in Blank_screenComponents:
+            if hasattr(thisComponent, "setAutoDraw"):
+                thisComponent.setAutoDraw(False)
+        thisExp.addData('Blank_screen.stopped', globalClock.getTime())
+        # using non-slip timing so subtract the expected duration of this Routine (unless ended on request)
+        if routineForceEnded:
+            routineTimer.reset()
+        else:
+            routineTimer.addTime(-0.500000)
         
         # --- Prepare to start Routine "FeedbackCode" ---
         continueRoutine = True
